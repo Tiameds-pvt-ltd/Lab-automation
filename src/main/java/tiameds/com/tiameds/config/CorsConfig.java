7b05be69388ccc70e5bde01eaca458d814a277df
abhishekritu01 <abhishekritu810219@gmail.com>
2024-12-13 21:18:07 +0530

    add cros origin

--- src/main/java/tiameds/com/tiameds/config/CorsConfig.java
+++ src/main/java/tiameds/com/tiameds/config/CorsConfig.java
@@ -13,7 +13,10 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("https://tiameds-lab-app.vercel.app/"));  // Allow frontend origin
+        configuration.setAllowedOrigins(List.of("https://tiameds-lab-app.vercel.app/",
+                "https://tiameds-lab-app-git-main-abhishek-kumars-projects-7cc8d4a1.vercel.app/",
+                "https://tiameds-lab-541wzlkz6-abhishek-kumars-projects-7cc8d4a1.vercel.app/"
+                ));  // Allow frontend origin
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));  // Allow these HTTP methods
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));  // Headers allowed in requests
         configuration.setAllowCredentials(true);  // Allow credentials like cookies
