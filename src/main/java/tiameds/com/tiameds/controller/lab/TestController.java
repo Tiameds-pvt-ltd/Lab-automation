e4cdba22bb7acf7d399fc5c279e27de1072ec043
abhishekritu01 <abhishekritu810219@gmail.com>
2024-12-25 17:22:01 +0530

    change in api response

--- src/main/java/tiameds/com/tiameds/controller/lab/TestController.java
+++ src/main/java/tiameds/com/tiameds/controller/lab/TestController.java
@@ -298,7 +298,6 @@ public class TestController {
             }
 
             // Check if the lab is active
-
             boolean isAccessible = labAccessableFilter.isLabAccessible(labId);
             if (isAccessible == false) {
                 return ApiResponseHelper.errorResponse("Lab is not accessible", HttpStatus.UNAUTHORIZED);
