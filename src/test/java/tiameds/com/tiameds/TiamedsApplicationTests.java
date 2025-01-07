61f3b8d70cd8cb71ceebe4fca9686abd227b8588
abhishek kumar <abhishek@tiameds.ai>
2025-01-07 22:26:22 +0530

    check9

--- src/test/java/tiameds/com/tiameds/TiamedsApplicationTests.java
+++ src/test/java/tiameds/com/tiameds/TiamedsApplicationTests.java
@@ -2,12 +2,13 @@ package tiameds.com.tiameds;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("prod")  // Ensure 'prod' profile is active
 class TiamedsApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
