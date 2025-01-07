7bbe1848ef80f64badaae0c996ea305e9b39d01a
abhishek kumar <abhishek@tiameds.ai>
2025-01-07 22:10:11 +0530

    check7

--- src/main/java/tiameds/com/tiameds/TiamedsApplication.java
+++ src/main/java/tiameds/com/tiameds/TiamedsApplication.java
@@ -2,16 +2,11 @@ package tiameds.com.tiameds;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.PropertySource;
-
 
 @SpringBootApplication
-@PropertySource("classpath:application.yml")
 public class TiamedsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TiamedsApplication.class, args);
     }
-
 }
